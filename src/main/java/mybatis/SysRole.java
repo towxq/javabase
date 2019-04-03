fcbbfe3cf541727e83733a5dc6092a17ab364e77
王星桥 <wang.xingqiao@sf-express.com>
2019-04-03 21:24:08 +0800

    20190403

---
+++ src/main/java/mybatis/SysRole.java
@@ -0,0 +1,51 @@
+package mybatis;
+
+import java.util.Date;
+
+public class SysRole {
+    private Long id;
+    private String roleName;
+    private String enabled;
+    private String createby;
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(String enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getCreateby() {
+        return createby;
+    }
+
+    public void setCreateby(String createby) {
+        this.createby = createby;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
