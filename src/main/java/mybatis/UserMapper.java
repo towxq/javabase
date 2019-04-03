d1581a938bfd0ab84a093251aa7aa6757770fd7d
王星桥 <wang.xingqiao@sf-express.com>
2019-04-03 21:18:43 +0800

    20190403

--- src/main/java/mybatis/UserMapper.java
+++ src/main/java/mybatis/UserMapper.java
@@ -7,4 +7,11 @@ public interface UserMapper {
     List<SysUser> selectAll();
     int insertUser(SysUser user);
     SysUser selectbyUserName(SysUser user);
+    int updateById(SysUser user);
+    List<SysUser> selectByIdList(List<Long> id);
+    int insertUserList(List<SysUser> sysUsers);
+    List<SysUser> selectUserAndRoleById(Long id);
+    List<SysUser> selectUserAndRoleById2(Long id);
+    List<SysUser> selectUserAndRoleById3(Long id);
+    List<SysUser> selectUserAndRoleById4(Long id);
 }
